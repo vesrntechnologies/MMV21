64c156fca72aa4dd33954435ad2a4dfa368c1c0e
vesrntechnologies <vennjoy@gmail.com>
2025-06-08 07:56:36 +0530

    Update questions.js

--- js/questions.js
+++ js/questions.js
@@ -553,7 +553,7 @@
       "Front air bag"
     ]
   },
-];
+]
 
   // you can uncomment the below codes and make duplicate as more as you want to add question
   // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
